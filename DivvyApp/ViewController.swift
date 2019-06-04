c1e0d5e4d535147095dd38a9e3f86e20f2142ece
bfehr11 <bfehr11@gmail.com>
2019-06-04 13:11:29 -0500

    Initial Commit

---
+++ DivvyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DivvyApp
+//
+//  Created by Brady Fehr on 6/4/19.
+//  Copyright © 2019 Brady Fehr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
